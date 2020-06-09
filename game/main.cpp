d0182ce15a0da1166768d934bd4cc8f6ad3eaada
yejineee <21700431@handong.edu>
2020-06-09 19:19:22 +0900

    Initial Commit

---
+++ game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by 양예진 on 2020/06/09.
+//  Copyright © 2020 YejinYang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
